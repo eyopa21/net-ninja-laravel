b708331b834226f3c66b63c855b36cb5c0468ac8
joba <jobtennis21@gmail.com>
2023-11-09 16:43:07 +0300

    adding route middlewares, protecting routes

--- routes/web.php
+++ routes/web.php
@@ -18,16 +18,16 @@ Route::get('/', function () {
 });
 
 //Route::get('/about', 'PizzaController@index');
-Route::get('/pizzas', [PizzaController::class, 'index']);
+Route::get('/pizzas', [PizzaController::class, 'index']) ->middleware('auth');
 
 Route::get('/pizzas/create', [PizzaController::class, 'create']);
 Route::post('/pizzas', [PizzaController::class, 'store']);
 
 //Route::get('pizzas/{id}', 'PizzaController@show');
-Route::get('/pizzas/{id}', [PizzaController::class, 'show']);
+Route::get('/pizzas/{id}', [PizzaController::class, 'show']) ->middleware('auth');
 
 
-Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy']);
+Route::delete('/pizzas/{id}', [PizzaController::class, 'destroy']) ->middleware('auth');
 
 Auth::routes();
 
